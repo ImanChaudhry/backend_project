e80f708e365414c81660ed99f4829c433a545f35
Aisha Mohamed <aisha_mo@hotmail.com>
2022-05-23 16:23:19 +0100

    created category enum

---
+++ src/main/java/com/bnta/banking_api/models/Category.java
@@ -0,0 +1,13 @@
+package com.bnta.banking_api.models;
+
+public enum Category {
+
+    BILLS,
+    ENTERTAINMENT,
+    EATING_OUT,
+    SHOPPING,
+    TRANSPORT,
+    HEALTH,
+    CHARITY,
+    OTHER
+}
