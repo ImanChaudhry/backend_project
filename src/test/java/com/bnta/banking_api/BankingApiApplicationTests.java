fa8dcd0f2c8e0981026db73279db117088f56909
Iman Chaudhry <i-man-735@hotmail.com>
2022-05-23 12:27:40 +0100

    created project

---
+++ src/test/java/com/bnta/banking_api/BankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnta.banking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
