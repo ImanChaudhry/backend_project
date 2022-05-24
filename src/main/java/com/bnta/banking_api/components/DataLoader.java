b69808036ccf3a685d0f2bc3c4c926e9d35f894a
bright-l-admin <bright-l-admin@BRIGHT-OS-14.cust.communityfibre.co.uk>
2022-05-24 11:01:33 +0100

    added repositories to dataloader

--- src/main/java/com/bnta/banking_api/components/DataLoader.java
+++ src/main/java/com/bnta/banking_api/components/DataLoader.java
@@ -1,21 +1,35 @@
 package com.bnta.banking_api.components;
 
-import com.bnta.banking_api.repositories.BasicAccountRepository;
+import com.bnta.banking_api.models.Payment;
+import com.bnta.banking_api.models.Subscription;
+import com.bnta.banking_api.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataLoader implements ApplicationRunner {
 
+
+    @Autowired
+    private AccountHolderRepository accountHolderRepository;
+
     @Autowired
     private BasicAccountRepository basicAccountRepository;
 
+    @Autowired
+    private JointAccountRepository jointAccountRepository;
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @Autowired
+    private SubscriptionRepository subscriptionRepository;
 
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-
     }
 }
