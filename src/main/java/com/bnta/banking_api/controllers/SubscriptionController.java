8a00e191df9f73269e74c7b0ac4f7c64eab16d07
bright-l-admin <bright-l-admin@BRIGHT-OS-14.cust.communityfibre.co.uk>
2022-05-24 15:10:09 +0100

    created controllers package and SubscriptionController Class

---
+++ src/main/java/com/bnta/banking_api/controllers/SubscriptionController.java
@@ -0,0 +1,9 @@
+package com.bnta.banking_api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class SubscriptionController {
+}
