ea2b02f02fbcca9f6f6720d8c8128ae53ae5b61f
Kim <87307041+JinleYe@users.noreply.github.com>
2022-05-26 12:32:39 +0100

    review account repository
    delete some merge comments

--- src/main/java/com/bnta/banking_api/repositories/AccountRepository.java
+++ src/main/java/com/bnta/banking_api/repositories/AccountRepository.java
@@ -3,7 +3,7 @@ package com.bnta.banking_api.repositories;
 import com.bnta.banking_api.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-<<<<<<< HEAD
+
 import java.util.List;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
@@ -13,8 +13,4 @@ public interface AccountRepository extends JpaRepository<Account, Long> {
     List<Account> findAccountByBalanceGreaterThan(double balance);
 
 
-=======
-public interface AccountRepository extends JpaRepository<Account, Long> {
-
->>>>>>> 9ac571282553c5d3289bac4da070765ae5d32d3d
 }
